1d050a775590c55c41d1f1f9424a3e0676432d1f
Pedro Lopes <pedroh.lopes772@gmail.com>
2025-04-28 23:27:49 +0000

    PIB per Capita e densidade populacional

--- Super Trunfo/supertrunfo.c
+++ Super Trunfo/supertrunfo.c
@@ -5,13 +5,13 @@ int main() {
     //Carta 1
 
     int populacao1, n_pontos_turisticos1;
-    float area1, PIB1;
+    float area1, PIB1, PIB_capita1, densidade_populacional1;
     char estado1[20], cidade1[50], codigo_carta1[5];
 
     //Carta 2
 
     int populacao2, n_pontos_turisticos2;
-    float area2, PIB2;
+    float area2, PIB2, PIB_capita2, densidade_populacional2;
     char estado2[20], cidade2[50], codigo_carta2[5];
 
 
@@ -48,9 +48,18 @@ int main() {
     printf("codigo da carta: %s\n", codigo_carta1);
     printf("Nome da Cidade: %s\n", cidade1);
     printf("Populacao: %d\n", populacao1);
-    printf("Area: %.0f km2\n", area1);
+    printf("Area: %.0f Km2\n", area1);
     printf("PIB: %.0f\n", PIB1);
     printf("Numero de pontos turisticos: %d\n", n_pontos_turisticos1);
+
+    //PIB percapita e Densidade populacional
+
+    PIB_capita1 = PIB1 / populacao1;
+    printf("PIB per capita: %.2f\n", PIB_capita1);
+
+    densidade_populacional1 = populacao1 / area1;
+    printf("Densidade populacional: %.2f\n", densidade_populacional1);
+
 
         //Cadastro Carta 2
 
@@ -85,8 +94,16 @@ int main() {
     printf("codigo da carta: %s\n", codigo_carta2);
     printf("Nome da Cidade: %s\n", cidade2);
     printf("Populacao: %d\n", populacao2);
-    printf("Area: %.0f km2\n", area2);
+    printf("Area: %.0f Km2\n", area2);
     printf("PIB: %.0f\n", PIB2);
     printf("Numero de pontos turisticos: %d\n", n_pontos_turisticos2);
+    
+    //PIB percapita e Densidade populacional
+
+    PIB_capita2 = PIB2 / populacao2;
+    printf("PIB per capita: %.2f\n", PIB_capita2);
+
+    densidade_populacional2 = populacao2 / area2;
+    printf("Densidade populacional: %.2f\n", densidade_populacional1);
     
 }
